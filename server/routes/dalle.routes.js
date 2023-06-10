6aef383e9762554df306e2fe0a230dd1da5e707a
konpeeyush <piyushksingh2589@gmail.com>
2023-06-10 16:16:57 +0530

    Adding dist files

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -20,16 +20,16 @@ router.route('/').post(async (req, res) => {
     try {
         const { prompt } = req.body;
 
-        const response = await openai.createImage(
-            {
-                prompt,
-                n: 1,
-                size: '1024x1024',
-                response_format: 'b64_json'
-            }
-        )
+        const response = await openai.createImage({
+            prompt,
+            n: 1,
+            size: '1024x1024',
+            response_format: 'b64_json'
+        });
+
         const image = response.data.data[0].b64_json;
-        res.status(200).json({ photo: image })
+
+        res.status(200).json({ photo: image });
     }
     catch (err) {
         console.log(err);
