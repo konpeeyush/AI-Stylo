5e5218f0bc000fbdf41c32004790cd8a42f7f80b
konpeeyush <piyushksingh2589@gmail.com>
2023-06-13 12:46:29 +0530

    Adding API link from render

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -24,17 +24,16 @@ router.route('/').post(async (req, res) => {
             prompt,
             n: 1,
             size: '1024x1024',
-            response_format: 'b64_json'
+            response_format: 'b64_json',
         });
 
-        const image = response.data.data[0].b64_json;
+        const image = response.data[0].b64_json;
 
         res.status(200).json({ photo: image });
-    }
-    catch (err) {
+    } catch (err) {
         console.log(err);
-        res.status(500).json({ message: 'Something went wrong!' })
+        res.status(500).json({ message: 'Something went wrong!' });
     }
-})
+});
 
 export default router;
